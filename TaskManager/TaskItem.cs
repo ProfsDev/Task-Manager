c53004c2a4dcd106ad1233c87995506d20968c67
ProfsDev <profsoftwaredevelopment@gmail.com>
2023-08-30 10:21:30 +0000

    added due data to add page and manage task page

--- TaskManager/TaskItem.cs
+++ TaskManager/TaskItem.cs
@@ -1,7 +1,7 @@
 public class TaskItem
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
-    public DateOnly date { get; set; }
+    public string Title { get; set; }
+    public DateOnly Date { get; set; }
     public bool IsDone { get; set; }
 }
